cf698703629f5377b3a987e4a9e1a7e5c34247a1
Kawano Kenichi <bppk28@gmail.com>
2021-02-14 06:13:32 +0000

    :sparkles: Add presenter class of product code

---
+++ accc/interface/cli/product_code/product_code_presenter.py
@@ -0,0 +1,26 @@
+from dataclasses import asdict
+from pathlib import Path
+
+from accc.core.usecase.product_code.product_code_output_data import (
+    ProductCodeOutputData,
+)
+from accc.core.usecase.product_code.product_code_presenter import ProductCodePresenter
+from black import Mode, format_file_contents
+from jinja2 import Environment, FileSystemLoader
+
+
+class ProductCodePresenter(ProductCodePresenter):
+    def __init__(self, path: Path):
+        self.path = path
+
+    def output(self, value: ProductCodeOutputData):
+        env = Environment(
+            loader=FileSystemLoader(Path(__file__).parent, encoding="utf-8")
+        )
+        template = env.get_template("product_code.j2")
+
+        rendered = template.render(asdict(value))
+
+        with open(self.path / value.file_name, "w") as f:
+            contents = format_file_contents(rendered, fast=True, mode=Mode())
+            f.write(contents)
--- tests/core/usecase/product_code/test_product_code_interactor.py
+++ tests/core/usecase/product_code/test_product_code_interactor.py
@@ -1,5 +1,6 @@
 from accc.core.usecase.product_code.product_code_input_data import ProductCodeInputData
 from accc.core.usecase.product_code.product_code_interactor import ProductCodeInteractor
+from accc.interface.cli.product_code.product_code_presenter import ProductCodePresenter
 from tests.core.usecase.product_code.mock_product_code_presenter import (
     MockProductCodePresenter,
 )
@@ -7,14 +8,16 @@ from tests.core.usecase.product_code.mock_product_code_presenter import (
 
 class Test_プロダクトコード用のインタラクタクラス:
     class Test_プロダクトコードを生成するcreateメソッド:
-        def test_解析可能な文字列を受け取る(self, tmp_path):
+        def test_Mock解析可能な文字列を受け取る(self, tmp_path):
             presenter = MockProductCodePresenter(tmp_path)
             interactor = ProductCodeInteractor(presenter)
 
-            input_data = ProductCodeInputData("hoge.py", ["N: int, M: int", "X: str"])
+            file_name = "test_file"
+            output_file_name = file_name + ".py"
+            input_data = ProductCodeInputData(file_name, ["N: int, M: int", "X: str"])
             interactor.create(input_data)
 
-            with open(tmp_path / "hoge.py", "r") as f:
+            with open(tmp_path / output_file_name, "r") as f:
                 assert f.readlines() == [
                     "N, M\n",
                     "X\n",
@@ -23,4 +26,30 @@ class Test_プロダクトコード用のインタラクタクラス:
                     "X = input()\n",
                     "N: int, M: int\n",
                     "X: str",
+                ]
+
+        def test_解析可能な文字列を受け取る(self, tmp_path):
+            presenter = ProductCodePresenter(tmp_path)
+            interactor = ProductCodeInteractor(presenter)
+
+            file_name = "test_file"
+            output_file_name = file_name + ".py"
+            input_data = ProductCodeInputData(file_name, ["N: int, M: int", "X: str"])
+            interactor.create(input_data)
+
+            with open(tmp_path / output_file_name, "r") as f:
+                assert f.readlines() == [
+                    "def algorithm(N: int, M: int, X: str):\n",
+                    "    return -1\n",
+                    "\n",
+                    "\n",
+                    "def main():\n",
+                    "    N, M = [int(x) for x in input().split()]\n",
+                    "    X = input()\n",
+                    "\n",
+                    "    print(algorithm(N, M, X))\n",
+                    "\n",
+                    "\n",
+                    'if __name__ == "__main__":\n',
+                    "    main()\n",
                 ]
